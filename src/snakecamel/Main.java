44f26837aea0f628d15abb70a114d76562f5dc55
Ri-1220 <g16936sr@gm.tsuda.ac.jp>
2018-05-16 11:08:22 +0900

    デバッガでプロジェクトの修正

---
+++ src/snakecamel/Main.java
@@ -0,0 +1,24 @@
+package snakecamel;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		String b;
+		if(Character.isUpperCase(a.charAt(0))) {
+		  b=SnakeCamelUtil.camelToSnakecase(a);
+		  System.out.println(b);
+		}
+		else if(Character.isLowerCase(a.charAt(0))){
+		  b = SnakeCamelUtil.snakeToCamelcase(a);
+		  System.out.println(b);	 
+		}
+		else if(a.matches(".*_.*")){
+			b = SnakeCamelUtil.snakeToCamelcase(a);	
+			System.out.println(b);	
+		}
+	}
+
+}
